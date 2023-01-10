4ed9f088a85426ea7492c5415d139064f2ad3504
ZiuChen <457353192@qq.com>
2023-01-10 17:32:11 +0800

    feat: 添加MediumZoom点击缩放图片

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,9 @@
 import DefaultTheme from 'vitepress/theme'
+import { onMounted, watch, nextTick, watchEffect } from 'vue'
+import { useRouter } from 'vitepress'
+import mediumZoom from 'medium-zoom'
 import Title from '../components/Title.vue'
+import './index.css'
 
 export default {
   ...DefaultTheme,
@@ -7,5 +11,13 @@ export default {
     DefaultTheme.enhanceApp(ctx)
     const { app } = ctx
     app.component('Title', Title)
+  },
+  setup() {
+    const router = useRouter()
+    watchEffect(() => {
+      // 将router.route.path作为依赖收集 首次访问即添加监听
+      const path = router.route.path
+      nextTick(() => mediumZoom('.main img', { background: 'var(--vp-c-bg)' }))
+    })
   }
 }
