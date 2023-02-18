c8432ee216cf046ecaf38b53bc044a1d72ebbc85
ZiuChen <457353192@qq.com>
2023-02-19 00:28:59 +0800

    fix: 生成侧边栏时排除资源文件夹

--- docs/.vitepress/scripts/generateSideBar.ts
+++ docs/.vitepress/scripts/generateSideBar.ts
@@ -4,14 +4,19 @@ import path from 'path'
 export default function generateSideBar() {
   const articles = fs.readdirSync(path.resolve(__dirname, '../../article'))
 
-  const sidebar = articles.map((article) => {
-    // 移除后缀 `.md`
-    const title = article.replace(/\.md$/, '')
-    return {
-      text: title,
-      link: `/article/${title}`
-    }
-  })
+  // 排除掉资源文件夹
+  const folders = ['assets']
+
+  const sidebar = articles
+    .filter((article) => !folders.includes(article))
+    .map((article) => {
+      // 移除后缀 `.md`
+      const title = article.replace(/\.md$/, '')
+      return {
+        text: title,
+        link: `/article/${title}`
+      }
+    })
 
   return sidebar
 }
